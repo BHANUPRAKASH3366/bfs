2404ac0488eee1396b5c6755c1983f7d265ffcf7
BHANUPRAKASH3366 <91242332+BHANUPRAKASH3366@users.noreply.github.com>
2022-05-08 21:16:58 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,64 +1,28 @@
-# Python3 Program to print BFS traversal
-# from a given source vertex. BFS(int s)
-# traverses vertices reachable from s.
-from collections import defaultdict
+graph = {
+  '5' : ['3','7'],
+  '3' : ['2', '4'],
+  '7' : ['8'],
+  '2' : [],
+  '4' : ['8'],
+  '8' : []
+}
 
-# This class represents a directed graph
-# using adjacency list representation
-class Graph:
+visited = [] # List for visited nodes.
+queue = []     #Initialize a queue
 
-	# Constructor
-	def __init__(self):
+def bfs(visited, graph, node): #function for BFS
+  visited.append(node)
+  queue.append(node)
 
-		# default dictionary to store graph
-		self.graph = defaultdict(list)
+  while queue:          # Creating loop to visit each node
+    m = queue.pop(0) 
+    print (m, end = " ") 
 
-	# function to add an edge to graph
-	def addEdge(self,u,v):
-		self.graph[u].append(v)
+    for neighbour in graph[m]:
+      if neighbour not in visited:
+        visited.append(neighbour)
+        queue.append(neighbour)
 
-	# Function to print a BFS of graph
-	def BFS(self, s):
-
-		# Mark all the vertices as not visited
-		visited = [False] * (max(self.graph) + 1)
-
-		# Create a queue for BFS
-		queue = []
-
-		# Mark the source node as
-		# visited and enqueue it
-		queue.append(s)
-		visited[s] = True
-
-		while queue:
-
-			# Dequeue a vertex from
-			# queue and print it
-			s = queue.pop(0)
-			print (s, end = " ")
-
-			# Get all adjacent vertices of the
-			# dequeued vertex s. If a adjacent
-			# has not been visited, then mark it
-			# visited and enqueue it
-			for i in self.graph[s]:
-				if visited[i] == False:
-					queue.append(i)
-					visited[i] = True
-
-# Driver code
-
-# Create a graph given in
-# the above diagram
-g = Graph()
-g.addEdge(0, 1)
-g.addEdge(0, 2)
-g.addEdge(1, 2)
-g.addEdge(2, 0)
-g.addEdge(2, 3)
-g.addEdge(3, 3)
-
-print ("Following is Breadth First Traversal"
-				" (starting from vertex 2)")
-g.BFS(2)
+# Driver Code
+print("Following is the Breadth-First Search")
+bfs(visited, graph, '5')    # function calling
